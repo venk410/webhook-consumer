ff97eae07183881df9c6dd91b6e1ddf3def502e7
Manchala, Venkatesh <Venkatesh.Manchala@STJUDE.ORG>
2020-02-04 11:18:49 -0600

    <Initial Commit> comnit1

---
+++ src/test/java/com/webhookpoc/webhookconsumer/WebhookConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webhookpoc.webhookconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebhookConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
